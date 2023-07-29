7c0422693cb29925e7c8e56b96ec4b9d12851dc4
夏沫花火zzz <xmcn@1l1.icu>
2023-07-29 14:12:24 +0800

    add git-scm

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,11 +1,17 @@
 <template>
     <el-card class="footer">
         <span>Copyright 2023 © XiaMoHuaHuo_CN.</span>
-        <pre>
-            　／l、
-            （ﾟ､ 。 ７
-            　l、 ~ヽ
-            　じしf_, )ノ
-        </pre>
+<pre>
+　／l、
+（ﾟ､ 。 ７
+　l、 ~ヽ
+　じしf_, )ノ
+</pre>
+        <a target="_blank" href="https://github.com/XiaMoHuaHuo-CN/DYCi">
+            <el-button class="button" text>
+                <ion-icon name="git-branch-outline"></ion-icon>
+                <el-text text>XiaMoHuaHuo-CN/DYCi</el-text>
+            </el-button>
+        </a>
     </el-card>
 </template>

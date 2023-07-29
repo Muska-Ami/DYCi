3ebba1e1cac42ec0bc9b59701b940cb19555069a
夏沫花火zzz <xmcn@1l1.icu>
2023-07-29 15:27:09 +0800

    fix overflow

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -8,7 +8,7 @@
 　じしf_, )ノ
 </pre>
         <a target="_blank" href="https://github.com/XiaMoHuaHuo-CN/DYCi">
-            <el-button class="button" text>
+            <el-button class="button">
                 <ion-icon name="git-branch-outline"></ion-icon>
                 <el-text text>XiaMoHuaHuo-CN/DYCi</el-text>
             </el-button>

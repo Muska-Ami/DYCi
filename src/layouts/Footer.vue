c6d5e035d6c645d3ab7594a0f5371fa7320409a7
夏沫花火zzz <xmcn@1l1.icu>
2023-10-05 22:32:51 +0800

    Update Copyright

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,16 +1,16 @@
 <template>
     <el-card class="footer">
-        <span>Copyright 2023 © XiaMoHuaHuo_CN.</span>
+        <span>Copyright 2023 © Muska_Ami.</span>
 <pre>
 　／l、
 （ﾟ､ 。 ７
 　l、 ~ヽ
 　じしf_, )ノ
 </pre>
-        <a target="_blank" href="https://github.com/XiaMoHuaHuo-CN/DYCi">
+        <a target="_blank" href="https://github.com/Muska-Ami/DYCi">
             <el-button class="button">
                 <ion-icon name="git-branch-outline"></ion-icon>
-                <el-text text>XiaMoHuaHuo-CN/DYCi</el-text>
+                <el-text text>Muska-Ami/DYCi</el-text>
             </el-button>
         </a>
     </el-card>
